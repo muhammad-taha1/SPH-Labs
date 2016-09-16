ebbe75d65764769af870add9801fcb4adf52b9d5
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-09-15 23:30:50 -0400

    fixed encoder issues; TODO check usage of n and dmin params

--- lab1/encoder.m
+++ lab1/encoder.m
@@ -1,3 +1,5 @@
+% This function creates a random message matrix of size KxK. Parity bits
+% are also added, making the matrix Kx2K (TODO: this should be n; check)
 function encodedMatrix = encoder (n, k, dmin)
 msg = [];
 for i =1 : k
@@ -6,40 +8,40 @@ for i =1 : k
     msg = [msg; msg_string];
 end
 
-msg
-% bitwise summation of msg
-% paritySum = [];
+% Parity bit logic: All rows in the matrix are added (using xor) except for
+% the row indicated by rowToNotAdd. This is the very last row for the very
+% first parity bits row, 2nd last row for the 2nd parity bits row and so on
 
-% for i = 1 : k
-%     pb = max(msg(:,i));
-%     paritySum = [paritySum pb];
-% end
-
+% Initialize rowToNotAdd as k, the last row in the message matrix
+rowToNotAdd = k;
 parityMatrix = [];
 for j = 1: k
-    parity = msg(1,:);
+    % parity row initialized as a row of zeros. Any row xor'd with 0's
+    % gives us the row. This is needed for the very first iteration of the
+    % for loop
+    parity = zeros(1, k);
+    
+    % Loop responisble for performing parity additions 
     for s = 1: k
-        % creating parity bits
-        
-        rowToSubs = k - s + 1; % j starts from 1, so add 1 to take last row into account
-        if (s == rowToSubs)
+        % creating parity bits        
+        if (s == rowToNotAdd)
             %dont add
         else
-            parity = bitor(parity, msg(s,:));
             %add
+            parity = xor(parity, msg(s,:));
         end
     
     end
+    % create a parityMatrix for each parity rows
     parityMatrix = [parityMatrix; parity];
-    % append parity bits to msg matrix
+    rowToNotAdd = rowToNotAdd - 1;
 end
-parityMatrix
-msg = [msg parityMatrix];
-%g= [1 0 0, 0 1 0, 0 0 1]; 
-% codeword = messages * g; 
-%E = (-0.5* ( rand (1, length) < Pe); 
-%encodedMatrix = 
 
+% append parityMatrix to msg matrix to create the final matrix according to
+% the specified encoding schema
+msg = [msg parityMatrix];
+
+% print out for debugging
 finalMsg = msg
 
 end
