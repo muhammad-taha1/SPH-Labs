e2007cac1ca11f2ae608e74a4e4f6abf29ff1833
Chrouk Kasem <shorouq.kassem@mail.mcgill.ca>
2016-10-24 21:54:44 -0400

    Viterbi Decoder

---
+++ lab2/EightFourFourCodeDecoder.m
@@ -0,0 +1,58 @@
+function DecodedOutput = EightFourFourCodeDecoder(receivedCodeword) 
+
+%Generator matrix is iddentity matrix with parity bits. 
+clc
+
+G = [1 0 0 0 1 1 1 0; 0 1 0 0 1 1 0 1; 0 0 1 0 1 0 1 1; 0 0 0 1 0 1 1 1]; 
+
+codebook = []; 
+
+for b1 = 0: 1
+    for b2 = 0 : 1
+        for b3 = 0 : 1
+            for b4 = 0 : 1
+            b = [b1 b2 b3 b4];
+            codebook = [codebook; mod(b*G, 2)];
+            end
+        end
+    end
+end
+
+cbSize = size(codebook); 
+distanceVector = [] ;
+contiansError = false; 
+squaredDistance = 0; 
+%% Loop to find minimum distanace 
+% Loops over the codebook and computes distance to received codeword by
+% taking sum(ri -ci)^2
+
+    for n = 1  : cbSize(1) 
+        distance = receivedCodeword(1,:) - codebook(n,:);
+        for k = 1 : 8 %number of columns in distance row 
+            if(abs(distance(k)) == 1) 
+                distanceVector = [distanceVector; inf]; 
+                break; 
+            else
+                containsError = true; 
+                squaredDistance = squaredDistance + distance(k)^2;
+            end 
+            if ((k == 8) && containsError) 
+                distanceVector = [distanceVector; squaredDistance]; 
+                squaredDistance = 0;
+            end 
+        end 
+    end 
+    
+%% Find codeword with minimum distance   
+    minIndex = find(distanceVector == min(distanceVector(:)));
+%% Match codeword
+% Use the minimum distance to extract codeword(s) from codebook 
+    outputCodeword = []; 
+    
+    for j =1 : size(minIndex) 
+        outputCodeword = [outputCodeword; codebook(minIndex(j),:)]; 
+    end
+    
+    DecodedOutput = outputCodeword; 
+    
+end
---
+++ lab2/ViterbiDecoder.m
@@ -0,0 +1,141 @@
+function decodedOutput = ViterbiDecoder(receivedRow)
+
+clc
+%%
+codebookEdges = [ 0 0 ; 1 1; 0 1; 1 0]  
+levelEdgeDistance = []; 
+numberOfPaths = [ 1 1 1 1; 2 2 2 2; 2 2 2 2; 1 1 1 1] ;
+%% Calculate the distance at edges 
+% The distances are stored in a 4 by 4 vector where the rows are stages of
+% trellis and columns are the edges
+    for k = 1 : 4 % number of stages in trellis for (8,4,4) Hamming Code 
+        for n = 1 : 4 % number of edge calcs in each level 
+            if( (k == 2) | (k == 3))
+                levelEdgeDistance(k,n) = (receivedRow(k+1)  - codebookEdges(n, 1))^2 +(receivedRow(k+3)  - codebookEdges(n, 2))^2;
+            elseif (k ==4)
+                levelEdgeDistance(k,n) = (receivedRow(k+3)  - codebookEdges(n, 1))^2 +(receivedRow(k+4)  - codebookEdges(n, 2))^2; 
+            else
+                levelEdgeDistance(k,n) = (receivedRow(k)  - codebookEdges(n, 1))^2 +(receivedRow(k+1)  - codebookEdges(n, 2))^2;
+            end
+        end
+    end
+%% Store edges distance in vectors for each stage
+% Each stage contains a varying number of edges, thus a vector for each
+% stage stores distance at each edge 
+    StageOneMinDist   = []; 
+    StageTwoMinDist   = []; 
+    StageThreeMinDist = []; 
+    StageFourMinDist  = []; 
+    
+    for l = 1 : 4 
+        StageOneMinDist = [StageOneMinDist ,levelEdgeDistance(1,l)];
+        if((l == 1) | (l == 2))
+            StageTwoMinDist = [StageTwoMinDist, levelEdgeDistance(2, 1)];
+            StageThreeMinDist = [StageThreeMinDist, levelEdgeDistance(3,1)];
+            StageTwoMinDist = [StageTwoMinDist, levelEdgeDistance(2, 2)];
+            StageThreeMinDist = [StageThreeMinDist, levelEdgeDistance(3,2)];
+        else 
+            StageTwoMinDist = [StageTwoMinDist, levelEdgeDistance(2, 3)];
+            StageThreeMinDist = [StageThreeMinDist, levelEdgeDistance(3,3)];
+            StageTwoMinDist = [StageTwoMinDist, levelEdgeDistance(2, 4)];
+            StageThreeMinDist = [StageThreeMinDist, levelEdgeDistance(3,4)];
+        
+        end
+            StageFourMinDist = [StageFourMinDist, levelEdgeDistance(4, l)];
+    end
+    
+shortestPathsVect = [];
+minDistSum = [];
+shortestPathCodeword = []; 
+
+%% Search Step
+% ADD-STORE-COMPARE at each state/stage of trellis taking the minimum sum
+% and storing it in  a vector at each stage for each state
+for stage = 1 : 4
+    for state = 1 : 4
+        if (numberOfPaths(stage,state) == 1)
+            if(stage == 1)
+                shortestPathsVect(stage,state) =  StageOneMinDist(state);
+                shortestPathCodeword(stage, state) = numberOfPaths(stage,state);
+            else
+                shortestPathsVect(stage,state) =  StageFourMinDist(state) + shortestPathsVect(stage - 1,state); 
+                shortestPathCodeword(stage, state) = numberOfPaths(stage,state);
+            end 
+        end
+        if (numberOfPaths(stage,state) == 2) 
+            if(stage == 2) 
+                minDistSum = [StageTwoMinDist(2*state - 1) + shortestPathsVect(stage - 1,state); StageTwoMinDist(2*state) + shortestPathsVect(stage - 1,state)]; 
+                minIndex = find(minDistSum == min(minDistSum(:)));
+                for s =1 : size(minIndex)
+                    shortestPathsVect(stage,state) =  minDistSum(minIndex(s)); 
+                    shortestPathCodeword(stage,state) = minIndex(s); 
+                end
+            else
+                minDistSum = [StageThreeMinDist(2*state - 1) + shortestPathsVect(stage - 1,state); StageThreeMinDist(2*state) + shortestPathsVect(stage - 1,state)];
+                minIndex = find(minDistSum == min(minDistSum(:)));
+                for s =1 : size(minIndex)
+                    shortestPathsVect(stage,state) =  minDistSum(minIndex(s)); 
+                    shortestPathCodeword(stage,state) = minIndex(s); 
+                end
+            end
+        end
+    end
+end
+%% Find min sum of the four states 
+% Take the path of that state
+
+shortestPaths = shortestPathsVect(4, :);
+CodewordPath = [];
+
+minPath = find(shortestPaths == min(shortestPaths(:)));
+
+for h = 1: size(minPath)
+    CodewordPath = [CodewordPath; shortestPathCodeword(:,minPath(h))]
+end
+
+codeword =[]; 
+%% Retrieve codeword for path
+% Using path vector, retrieve codeword that matches this path by  matching
+% the edges with the symbols. 
+
+% This is the first stage codeword 
+if(minPath == 1) 
+        codeword = [codeword, 0 0]; 
+elseif(minPath == 2)
+        codeword = [codeword, 1 1];
+elseif(minPath == 3) 
+        codeword = [codeword, 0 1];
+else 
+        codeword = [codeword, 1 0];
+end 
+%Retrieves symbols for 2nd, 3rd, and 4th stages
+for j = 2: 4
+    if(minPath == 1)  
+        if(CodewordPath(j) == 1)
+            codeword = [codeword, 0 0]; 
+        else
+            codeword = [codeword, 1 1]; 
+        end
+    elseif (minPath == 2) 
+        if(CodewordPath(j) == 1)
+            codeword = [codeword, 0 0]; 
+        else
+            codeword = [codeword, 1 1]; 
+        end
+    elseif (minPath == 3)        
+        if(CodewordPath(j) == 1)
+            codeword = [codeword, 0 1]; 
+        else
+            codeword = [codeword, 1 0]; 
+        end
+    else
+        if(CodewordPath(i) == 1)
+            codeword = [codeword, 0 1]; 
+        else
+            codeword = [codeword, 1 0]; 
+        end
+    end
+end
+%% Output codeword
+decodedOutput = codeword;
+end
