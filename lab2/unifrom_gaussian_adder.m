12b88a4556e3e0b5907e0e2633ee019295d96f03
Chrouk Kasem <shorouq.kassem@mail.mcgill.ca>
2016-11-09 11:06:41 -0500

    final report

--- lab2/unifrom_gaussian_adder.m
+++ lab2/unifrom_gaussian_adder.m
@@ -2,7 +2,7 @@ function unifrom_gaussian_adder
 
 uniform_noise = [];
 for k = 1:1000
-        uniform_noise =  [sum(-1 + rand(6,1)*2)/6 uniform_noise];
+        uniform_noise =  [sum(-1 + rand(6,1)*2) uniform_noise];
 end
 figure
 hist(uniform_noise)
