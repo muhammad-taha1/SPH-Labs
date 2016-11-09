072ef36b7832f81f1ade105b6b6ee53f8a2a2e3d
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-11-08 19:18:32 -0500

    report update

--- lab2/speechProcess.m
+++ lab2/speechProcess.m
@@ -27,14 +27,14 @@ h = audioread('h.wav');
 
 % part c
 % shifted h => append col of zeros by shifted amout before h
-% h_shift = [zeros(3000,1);h];
-% h_echo = [h;zeros(3000,1)] + h_shift;
-% x_conv = conv(x, h_echo);
-%sound(x_conv, 16000);
+h_shift = [zeros(3000,1);h];
+h_echo = [h;zeros(3000,1)] + h_shift;
+x_conv = conv(x, h_echo);
+sound(x_conv, 16000);
 
 % part d
-% x_flip = flipud(x);
-%sound(x_flip, 16000);
+x_flip = flipud(x);
+sound(x_flip, 16000);
 
 % part e
 %sound(x, 13000);
@@ -44,41 +44,41 @@ h = audioread('h.wav');
 %sound(x, 20000);
 
 % part f
-% x_sub_2 = [];
-% 
-% for i = 1 : length(x)
-%     if (mod(i, 2) == 0)
-%         x_sub_2 = [x_sub_2; x(i)];
-%     end
-% end
-% 
-% x_sub_3 = [];
-% for i = 1 : length(x)
-%     if (mod(i, 3) == 0)
-%         x_sub_3 = [x_sub_3; x(i)];
-%     end
-% end
-% 
-% x_sub_4 = [];
-% for i = 1 : length(x)
-%     if (mod(i, 4) == 0)
-%         x_sub_4 = [x_sub_4; x(i)];
-%     end
-% end
-% 
-% x_sub_5 = [];
-% for i = 1 : length(x)
-%     if (mod(i, 5) == 0)
-%         x_sub_5 = [x_sub_5; x(i)];
-%     end
-% end
-% 
-% x_sub_10 = [];
-% for i = 1 : length(x)
-%     if (mod(i, 10) == 0)
-%         x_sub_10 = [x_sub_10; x(i)];
-%     end
-% end
+x_sub_2 = [];
+
+for i = 1 : length(x)
+    if (mod(i, 2) == 0)
+        x_sub_2 = [x_sub_2; x(i)];
+    end
+end
+
+x_sub_3 = [];
+for i = 1 : length(x)
+    if (mod(i, 3) == 0)
+        x_sub_3 = [x_sub_3; x(i)];
+    end
+end
+
+x_sub_4 = [];
+for i = 1 : length(x)
+    if (mod(i, 4) == 0)
+        x_sub_4 = [x_sub_4; x(i)];
+    end
+end
+
+x_sub_5 = [];
+for i = 1 : length(x)
+    if (mod(i, 5) == 0)
+        x_sub_5 = [x_sub_5; x(i)];
+    end
+end
+
+x_sub_10 = [];
+for i = 1 : length(x)
+    if (mod(i, 10) == 0)
+        x_sub_10 = [x_sub_10; x(i)];
+    end
+end
 
 
 %sound(x_sub_10, 16000/10);
@@ -106,63 +106,62 @@ h = audioread('h.wav');
 % Divide the signal by 2^(quantization level - 1) to normalize
 % Plot and listen to resulting quantized signal
 
-% oneBitQuant = x;
-% for i = 1 : length(oneBitQuant)
-%     if (oneBitQuant(i) > 0)
-%         oneBitQuant(i) = 1;
-%     end
-%     if (oneBitQuant(i) < 0)
-%         oneBitQuant(i) = -1;
-%     end
-% end
-% oneBitQuant = oneBitQuant/2^1;
-% stem(oneBitQuant);
-% sound(oneBitQuant, 16000);
+oneBitQuant = x;
+for i = 1 : length(oneBitQuant)
+    if (oneBitQuant(i) > 0)
+        oneBitQuant(i) = 1;
+    end
+    if (oneBitQuant(i) < 0)
+        oneBitQuant(i) = -1;
+    end
+end
+oneBitQuant = oneBitQuant/2^1;
+stem(oneBitQuant);
+sound(oneBitQuant, 16000);
 
-% twoBitQuant = round(x*2^2);
-% for i = 1 : length(twoBitQuant)
-%     if (twoBitQuant(i) > (2^1 - 1))
-%         twoBitQuant(i) = 1;
-%     end
-%     if (twoBitQuant(i) < -(2^1 - 1))
-%         twoBitQuant(i) = -1;
-%     end
-%     
-%     if (x(i) < 0 && twoBitQuant(i) == 0)
-%         twoBitQuant(i) = -0.5;
-%     end
-%     
-%     if (x(i) >= 0 && twoBitQuant(i) == 0)
-%         twoBitQuant(i) = 0.5;
-%     end
-% end
-% unique(twoBitQuant)
-% twoBitQuant = twoBitQuant/2^2;
-% stem(twoBitQuant);
-% sound(twoBitQuant, 16000);
+twoBitQuant = round(x*2^2);
+for i = 1 : length(twoBitQuant)
+    if (twoBitQuant(i) > (2^1 - 1))
+        twoBitQuant(i) = 1;
+    end
+    if (twoBitQuant(i) < -(2^1 - 1))
+        twoBitQuant(i) = -1;
+    end
+    
+    if (x(i) < 0 && twoBitQuant(i) == 0)
+        twoBitQuant(i) = -0.5;
+    end
+    
+    if (x(i) >= 0 && twoBitQuant(i) == 0)
+        twoBitQuant(i) = 0.5;
+    end
+end
+twoBitQuant = twoBitQuant/2^2;
+stem(twoBitQuant);
+sound(twoBitQuant, 16000);
 
 
-% fourBitQuant = round(x*2^4);
-% for i = 1 : length(fourBitQuant)
-%     if (fourBitQuant(i) > (2^3 - 1))
-%         fourBitQuant(i) = 7;
-%     end
-%     if (fourBitQuant(i) < -(2^3 - 1))
-%         fourBitQuant(i) = -7;
-%     end
-%     
-%     if (x(i) < 0 && fourBitQuant(i) == 0)
-%         fourBitQuant(i) = -0.5;
-%     end
-%     
-%     if (x(i) >= 0 && fourBitQuant(i) == 0)
-%         fourBitQuant(i) = 0.5;
-%     end
-% end
-% unique(fourBitQuant)
-% fourBitQuant = fourBitQuant/2^4;
-% stem(fourBitQuant);
-% sound(fourBitQuant, 16000);
+fourBitQuant = round(x*2^4);
+for i = 1 : length(fourBitQuant)
+    if (fourBitQuant(i) > (2^3 - 1))
+        fourBitQuant(i) = 7;
+    end
+    if (fourBitQuant(i) < -(2^3 - 1))
+        fourBitQuant(i) = -7;
+    end
+    
+    if (x(i) < 0 && fourBitQuant(i) == 0)
+        fourBitQuant(i) = -0.5;
+    end
+    
+    if (x(i) >= 0 && fourBitQuant(i) == 0)
+        fourBitQuant(i) = 0.5;
+    end
+end
+unique(fourBitQuant)
+fourBitQuant = fourBitQuant/2^4;
+stem(fourBitQuant);
+sound(fourBitQuant, 16000);
 
 EightBitQuant = round(x*2^8);
 for i = 1 : length(EightBitQuant)
