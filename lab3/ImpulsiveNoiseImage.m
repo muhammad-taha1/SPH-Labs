46b272f1ac9cc6f54c964a7aa9f6478890324cf9
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-11-21 17:15:57 -0500

    1c-d

---
+++ lab3/ImpulsiveNoiseImage.m
@@ -0,0 +1,15 @@
+function ImpulsiveNoiseImage()
+
+img = imread('Baboon__grey_scale.jpg');
+img = rgb2gray(img);
+img = cast(img, 'double');
+%img = img./max(max(img));
+[m, n] = size(img);
+%imshow(img)
+impulseProb = 10;
+noise = (rand(m, n) < impulseProb/100)*max(max(img));
+
+result = img + noise;
+result = result./max(max(result));
+imshow(result);
+end
