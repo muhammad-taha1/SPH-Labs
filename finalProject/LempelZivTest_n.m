e2604ff22bb0dd96455021fc14e50588148ae56a
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-12-14 00:20:33 -0500

    markov

--- finalProject/LempelZivTest_n.m
+++ finalProject/LempelZivTest_n.m
@@ -1,89 +1,94 @@
 function LempelZivTest_n()
-n = [2, 5, 10, 50, 100];
+n = [2, 5, 10, 20, 25, 50, 80, 100];
 compRatio1 = [];
 compRatio2 = [];
 compRatio3 = [];
-avgCompRatio1 = []; 
+avgCompRatio1 = [];
 avgCompRatio2 = [];
-avgCompRatio3 = []; 
+avgCompRatio3 = [];
+doMarkov = 1;
 
+p0 = 0.90;
+p1 = 1 - p0;
+p1AtOne = 0.8;
 % only constraint here is that the input length should be divisible by n
 for i= 1: length(n)
-    inputSizes = [100000];
-    for j = 1: 10
-        input = rand(1, inputSizes) > 0.95;
-%% compute prob for each symbol, for Hx calculation. Assume that the input
-% only has two: symbols, 0 and 1.
-
-p0 = 0;
-p1 = 0;
-for k = 1 : length(input)
-    if (input(k) == 0)
-        p0 = p0 + 1;
-    else
-        p1 = p1 + 1;
+    inputSizes = [10^4];
+    for j = 1: 100
+        if (doMarkov)
+            input = MarkovSource(inputSizes, p0, p1AtOne);%
+        else
+            input = rand(1, inputSizes) > p0;
+        end
+        %% compute prob for each symbol, for Hx calculation. Assume that the input
+        % only has two: symbols, 0 and 1.
+        
+        % Hx formula taken from dp1 report
+        if (doMarkov)
+            ProbZeroAtOne = 1 - p1AtOne; 
+            ProbOneAtZero = 1 - p0;
+            
+            pieZero = ProbZeroAtOne / (ProbZeroAtOne + ProbOneAtZero);
+            pieOne = 1 - pieZero;
+            Hx = pieZero * (p0 * log2(1/p0) + ProbOneAtZero * log2(1/ProbOneAtZero)) + pieOne * (p1AtOne * log2(1/p1AtOne) + ProbZeroAtOne * log2(1/ProbZeroAtOne));
+        else
+            Hx = p0*log2(1/p0) + p1*log2(1/p1);
+        end
+        
+        if ((p0 == 1) || (p1 == 0))
+            Hx = 0;
+        end
+        % W is the window size according to formula provided by Proff
+        w = n(i)^2*(2^(n(i)*Hx));
+        w3 = 0;
+        rem =(3 - mod(ceil(2^(n(i)*Hx)), n(i))) + ceil( 2^(n(i)*Hx));
+        if (mod(w,n(i)) == 0)
+            w3 = w;
+        else
+            w3 = n(i)^2 * rem;
+        end
+        % ceil this value, can't have a window size in decimals
+        %w = ceil(w);
+        %w = 9;
+        [encoded1, P_match1] = LempelZivEncoder1(input, n(i), w);
+        decoded1 = LempelZivDecoder1(encoded1, n(i), w);
+        
+        compRatio1(j) = length(encoded1) / length(input);
+        
+        if (~isequal(input,decoded1))
+            fprintf('fix me 1!');
+        end
+        
+        [encoded2, P_match2] = LempelZivEncoder2(input, n(i), w);
+        decoded2 = LempelZivDecoder2(encoded2, n(i), w);
+        
+        compRatio2(j) = length(encoded2) / length(input);
+        
+        if (~isequal(input,decoded2))
+            fprintf('fix me 2!');
+        end
+        
+        [encoded3, P_match3] = LempelZivEncoder3(input, n(i),w3);
+        decoded3 = LempelZivDecoder3(encoded3, n(i), w3);
+        
+        compRatio3(j) = length(encoded3) / length(input);
+        
+        if (~isequal(input,decoded3))
+            fprintf('fix me 3!');
+        end
     end
-end
-
-p0 = p0/length(input);
-p1 = p1/length(input);
-
-% Hx formula taken from dp1 report
-Hx = p0*log2(1/p0) + p1*log2(1/p1);
-
-if ((p0 == 1) || (p1 == 0)) 
-    Hx = 0; 
-end 
-% W is the window size according to formula provided by Proff
-w = n(i)^2*(2^(n(i)*Hx));
-w3 = 0; 
-rem =(3 - mod(ceil(2^(n(i)*Hx)), n(i))) + ceil( 2^(n(i)*Hx));
-if (mod(w,n(i)) == 0) 
-   w3 = w; 
-else
-    w3 = n(i)^2 * rem;
-end
-% ceil this value, can't have a window size in decimals
-%w = ceil(w);
-%w = 9; 
-[encoded1, P_match1] = LempelZivEncoder1(input, n(i), w);
-decoded1 = LempelZivDecoder1(encoded1, n(i), w);
-
-compRatio1(j) = length(encoded1) / length(input); 
-
-if (~isequal(input,decoded1))
-    fprintf('fix me 1!');
-end
-
-[encoded2, P_match2] = LempelZivEncoder2(input, n(i), w);
-decoded2 = LempelZivDecoder2(encoded2, n(i), w);
-
-compRatio2(j) = length(encoded2) / length(input); 
-
-if (~isequal(input,decoded2))
-    fprintf('fix me 2!');
-end
-
-[encoded3, P_match3] = LempelZivEncoder3(input, n(i),w3);
-decoded3 = LempelZivDecoder3(encoded3, n(i), w3);
-
-compRatio3(j) = length(encoded3) / length(input); 
-
-if (~isequal(input,decoded3))
-    fprintf('fix me 3!');
-end
-    end
-    avgCompRatio1(i)= mean(compRatio1); 
+    avgCompRatio1(i)= mean(compRatio1);
     avgCompRatio2(i) = mean(compRatio2);
     avgCompRatio3(i) = mean(compRatio3);
     fprintf('Done!');
 end
 %% Plots against sizes of n
 figure
-stem(n, avgCompRatio1, 'filled');
-hold
-stem(n, avgCompRatio2, 'filled', 'red');
+stem(n, avgCompRatio1, 'x');
+hold on
+stem(n, avgCompRatio2, '*', 'red');
+hold on
 stem(n, avgCompRatio3, 'filled', 'green');
-xlabel('Length of n block'); 
-ylabel('Compression Efficiency'); 
+xlabel('Length of n block');
+ylabel('Compression Efficiency');
 end
