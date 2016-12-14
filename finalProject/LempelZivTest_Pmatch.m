624442ab7fe8c276d4072c9e4c71caa33f190c18
Chrouk Kasem <shorouq.kassem@mail.mcgill.ca>
2016-12-13 23:09:41 -0500

    Pmatch Test simulation

--- finalProject/LempelZivTest_Pmatch.m
+++ finalProject/LempelZivTest_Pmatch.m
@@ -1,16 +1,16 @@
 function LempelZivTest_n()
 n = [2, 5, 10, 50, 100];
-compRatio1 = [];
-compRatio2 = [];
-compRatio3 = [];
-avgCompRatio1 = []; 
-avgCompRatio2 = [];
-avgCompRatio3 = []; 
+Pmatch1 = [];
+Pmatch2 = [];
+Pmatch3 = [];
+avgPmatch1 = []; 
+avgPmatch2 = [];
+avgPmatch3 = []; 
 
 % only constraint here is that the input length should be divisible by n
 for i= 1: length(n)
     inputSizes = [100000];
-    for j = 1: 10
+    for j = 1: 1
         input = rand(1, inputSizes) > 0.95;
 %% compute prob for each symbol, for Hx calculation. Assume that the input
 % only has two: symbols, 0 and 1.
@@ -49,7 +49,7 @@ end
 [encoded1, P_match1] = LempelZivEncoder1(input, n(i), w);
 decoded1 = LempelZivDecoder1(encoded1, n(i), w);
 
-compRatio1(j) = length(encoded1) / length(input); 
+Pmatch1(j) = Pmatch_1; 
 
 if (~isequal(input,decoded1))
     fprintf('fix me 1!');
@@ -58,7 +58,7 @@ end
 [encoded2, P_match2] = LempelZivEncoder2(input, n(i), w);
 decoded2 = LempelZivDecoder2(encoded2, n(i), w);
 
-compRatio2(j) = length(encoded2) / length(input); 
+Pmatch2(j) = P_match2; 
 
 if (~isequal(input,decoded2))
     fprintf('fix me 2!');
@@ -67,23 +67,23 @@ end
 [encoded3, P_match3] = LempelZivEncoder3(input, n(i),w3);
 decoded3 = LempelZivDecoder3(encoded3, n(i), w3);
 
-compRatio3(j) = length(encoded3) / length(input); 
+Pmatch3(j) = P_match3; 
 
 if (~isequal(input,decoded3))
     fprintf('fix me 3!');
 end
     end
-    avgCompRatio1(i)= mean(compRatio1); 
-    avgCompRatio2(i) = mean(compRatio2);
-    avgCompRatio3(i) = mean(compRatio3);
+    avgPmatch1(i)= mean(Pmatch1); 
+    avgPmatch2(i) = mean(Pmatch2);
+    avgPmatch3(i) = mean(Pmatch3);
     fprintf('Done!');
 end
 %% Plots against sizes of n
 figure
-stem(n, avgCompRatio1, 'filled');
+stem(n, avgPmatch1, 'filled');
 hold
-stem(n, avgCompRatio2, 'filled', 'red');
-stem(n, avgCompRatio3, 'filled', 'green');
+stem(n, avgPmatch2, 'filled', 'red');
+stem(n, avgPmatch3, 'filled', 'green');
 xlabel('Length of n block'); 
 ylabel('Compression Efficiency'); 
 end
