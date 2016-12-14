aac403357b97b69b62bb35d29712d4c91c4c76fb
Chrouk Kasem <shorouq.kassem@mail.mcgill.ca>
2016-12-13 22:27:58 -0500

    Simulations with b

--- finalProject/LempelZivTest_n.m
+++ finalProject/LempelZivTest_n.m
@@ -1,7 +1,5 @@
-function LempelZivTest()
-n = [3, 6, 10, 50, 100] 
-%inputSizes = [10*n, 100*n, 1000*n, 10000*n, 100000*n, 1000000*n];
-
+function LempelZivTest_n()
+n = [2, 5, 10, 50, 100];
 compRatio1 = [];
 compRatio2 = [];
 compRatio3 = [];
@@ -10,13 +8,12 @@ avgCompRatio2 = [];
 avgCompRatio3 = []; 
 
 % only constraint here is that the input length should be divisible by n
-for i= 1: length(inputSizes)
-% Comment this out when testing different sizes of n. 
-    %inputSizes = [10000*n(i)];
-    for j = 1: 1000
-        input = rand(1, inputSizes(i)) > 0.95;
-% compute prob for each symbol, for Hx calculation. Assume that the input
-% only has two       symbols, 0 and 1.
+for i= 1: length(n)
+    inputSizes = [10000];
+    for j = 1: 10
+        input = rand(1, inputSizes) > 0.95;
+%% compute prob for each symbol, for Hx calculation. Assume that the input
+% only has two: symbols, 0 and 1.
 
 p0 = 0;
 p1 = 0;
@@ -38,19 +35,19 @@ if ((p0 == 1) || (p1 == 0))
     Hx = 0; 
 end 
 % W is the window size according to formula provided by Proff
-w = n^2*(2^(n*Hx));
+w = n(i)^2*(2^(n(i)*Hx));
 w3 = 0; 
-rem =(3 - mod(ceil(2^(n*Hx)), n)) + ceil( 2^(n*Hx));
-if (mod(w,n) == 0) 
+rem =(3 - mod(ceil(2^(n(i)*Hx)), n(i))) + ceil( 2^(n(i)*Hx));
+if (mod(w,n(i)) == 0) 
    w3 = w; 
 else
-    w3 = n^2 * rem;
+    w3 = n(i)^2 * rem;
 end
 % ceil this value, can't have a window size in decimals
 %w = ceil(w);
 %w = 9; 
-[encoded1, P_match1] = LempelZivEncoder1(input, n, w);
-decoded1 = LempelZivDecoder1(encoded1, n, w);
+[encoded1, P_match1] = LempelZivEncoder1(input, n(i), w);
+decoded1 = LempelZivDecoder1(encoded1, n(i), w);
 
 compRatio1(j) = length(encoded1) / length(input); 
 
@@ -58,8 +55,8 @@ if (~isequal(input,decoded1))
     fprintf('fix me 1!');
 end
 
-[encoded2, P_match2] = LempelZivEncoder2(input, n, w);
-decoded2 = LempelZivDecoder2(encoded2, n, w);
+[encoded2, P_match2] = LempelZivEncoder2(input, n(i), w);
+decoded2 = LempelZivDecoder2(encoded2, n(i), w);
 
 compRatio2(j) = length(encoded2) / length(input); 
 
@@ -67,8 +64,8 @@ if (~isequal(input,decoded2))
     fprintf('fix me 2!');
 end
 
-[encoded3, P_match3] = LempelZivEncoder3(input, n,w3);
-decoded3 = LempelZivDecoder3(encoded3, n, w3);
+[encoded3, P_match3] = LempelZivEncoder3(input, n(i),w3);
+decoded3 = LempelZivDecoder3(encoded3, n(i), w3);
 
 compRatio3(j) = length(encoded3) / length(input); 
 
@@ -81,10 +78,12 @@ end
     avgCompRatio3(i) = mean(compRatio3);
     fprintf('Done!');
 end
-figure 
-plot(inputSizes, avgCompRatio1);
+%% Plots against sizes of n
+figure
+stem(n, avgCompRatio1, 'filled');
 hold
-plot(inputSizes, avgCompRatio2);
-hold
-plot(inputSizes, avgCompRatio3);
+stem(n, avgCompRatio2, 'filled', 'red');
+stem(n, avgCompRatio3, 'filled', 'green');
+xlabel('Length of n block'); 
+ylabel('Compression Efficiency'); 
 end
